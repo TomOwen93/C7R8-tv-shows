b54c8bffc2e1ff599c95c69d845671ac1c31257e
oskartyrala <oskartyrala@protonmail.com>
2023-07-24 16:31:12 +0200

    Add episode component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,46 @@
 import "./App.css";
 import episodes from "../episodes.json";
+import { Episode } from "./Episode";
 console.log(`Imported ${episodes.length} episode(s)`);
 console.log(`First episode's name is ${episodes[0].name}`);
 
+export interface IEpisode {
+    id: number;
+    url?: string;
+    name: string;
+    season: number;
+    number: number;
+    type?: string;
+    airdate?: string;
+    airtime?: string;
+    airstamp?: string;
+    rating?: { average: number };
+    runtime?: number;
+    image: {
+      medium: string;
+      original: string;
+    };
+    summary: string;
+    _links?: { self: { href: string } };
+  }
+
 function App() {
+
+    const allEpisodes = episodes.map((episode) => {
+        return <Episode 
+                    id={episode.id}
+                    name={episode.name}
+                    season={episode.season}
+                    number={episode.number}
+                    image={episode.image}
+                    summary={episode.summary}
+                />
+    })
+
     return (
         <div className="App">
             <h1>Hello Neill from Oskar and Tom</h1>
+            {allEpisodes}
         </div>
     );
 }
---
+++ src/components/Episode.tsx
@@ -0,0 +1,5 @@
+import { IEpisode } from "./App";
+
+export function Episode({id, name, season, number, image, summary}: IEpisode): JSX.Element {
+    return <div>My Component {`${id} ${name} ${season} ${number} ${image} ${summary}`}</div>;
+}
