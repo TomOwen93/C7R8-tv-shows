ff05296c106a270967c970fe013cf8840f09967a
TomOwen93 <mr.tsowen@gmail.com>
2023-07-24 13:53:47 +0100

    initial deploy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,13 +1,9 @@
-import { MyComponent } from "./MyComponent";
 import "./App.css";
-import { greet } from "../greet";
 
 function App() {
     return (
         <div className="App">
-            <MyComponent />
-
-            {greet("World")}
+            <h1>Hello Neill from Oskar and Tom</h1>
         </div>
     );
 }
