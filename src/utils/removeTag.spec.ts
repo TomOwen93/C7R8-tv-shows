d411b048a5cf286977fe5a155ffaef8b4810443e
oskartyrala <oskartyrala@protonmail.com>
2023-07-25 11:21:38 +0200

    format and lint

--- src/utils/removeTag.spec.ts
+++ src/utils/removeTag.spec.ts
@@ -1,10 +1,12 @@
 import removeTags from "./removeTags";
 
 test("Removes HTML tags", () => {
-  expect(removeTags("<p>test</p>")).toEqual("test")
-  expect(removeTags("<p>test</p></br>")).toEqual("test")
-  expect(removeTags("<img src=''>test")).toEqual("test")
-  expect(removeTags("test")).toEqual("test")
-  expect(removeTags("")).toEqual("")
-  expect(removeTags("<div><p>test </p> more test </div>")).toEqual("test  more test ")
-})
+    expect(removeTags("<p>test</p>")).toEqual("test");
+    expect(removeTags("<p>test</p></br>")).toEqual("test");
+    expect(removeTags("<img src=''>test")).toEqual("test");
+    expect(removeTags("test")).toEqual("test");
+    expect(removeTags("")).toEqual("");
+    expect(removeTags("<div><p>test </p> more test </div>")).toEqual(
+        "test  more test "
+    );
+});
